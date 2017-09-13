bfd0860b5dc8d2dffcbb34cab17b59843a955814
Thiago Dallacqua <thiagodallacqua@outlook.com>
2017-09-13 13:16:28 -0300

    added memjs and aditional logic to memcached for production

--- servicos/MemcachedClient.js
+++ servicos/MemcachedClient.js
@@ -1,13 +1,28 @@
-var memcached = require('memcached');
+var memcached = require('memjs');
+var memcachedLocal = require('memcached');
 
 function createMemcachedClient() {
-  var client = new memcached('localhost:11211', {
-    retries: 10, //número de tentativas de consultas no cluster
-    retry: 10000, //tempo, em milisegundos, para tentar consultar um nó do cluster
-    remove: true //remover, ou não, um nó morto no cluster
-  });
+  if (!process.env.NODE_ENV) {
+    var client = new memcachedLocal('localhost:11211', {
+      retries: 10, //número de tentativas de consultas no cluster
+      retry: 10000, //tempo, em milisegundos, para tentar consultar um nó do cluster
+      remove: true //remover, ou não, um nó morto no cluster
+    });
 
-  return client;
+    return client
+  }
+
+  if (process.env.NODE_ENV == 'production') {
+    var client = new memcached(process.env.MEMCACHEDCLOUD_SERVERS, {
+      username: process.env.MEMCACHEDCLOUD_USERNAME,
+      password: process.env.MEMCACHEDCLOUD_PASSWORD,
+      retries: 10, //número de tentativas de consultas no cluster
+      retry: 10000, //tempo, em milisegundos, para tentar consultar um nó do cluster
+      remove: true //remover, ou não, um nó morto no cluster
+    });
+
+    return client
+  }
 }
 
 module.exports = function() {
