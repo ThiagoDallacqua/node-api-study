cd450a0fa28bfc76ec658ece8d097580d68c9086
Thiago Dallacqua <thiagodallacqua@outlook.com>
2017-09-13 11:03:36 -0300

    prepared the app for deployment

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 var app = require('./config/express')();
+var porta = process.env.PORT || 3000;
 
 app.listen(3000, function() {
-  console.log('Servidor rodando na porta 3000');
+  var host = app.address().address;
+  var port = app.address().port;
+  
+  console.log(`Servidor rodando no endereço ${host}:${port}`);
 });
