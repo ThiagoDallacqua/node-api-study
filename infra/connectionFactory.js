ac7b0a5d33f6921c6d10b574f767ee446ccfbb0f
Thiago Dallacqua <thiagodallacqua@outlook.com>
2017-09-13 10:53:23 -0300

    added production variables to database

--- infra/connectionFactory.js
+++ infra/connectionFactory.js
@@ -1,12 +1,23 @@
 var mysql = require('mysql');
 
 function createDBConnection(app) {
-  return mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '1234',
-    database: 'payfast'
-  });
+  if (!process.env.NODE_ENV) {
+    return mysql.createConnection({
+      host: 'localhost',
+      user: 'root',
+      password: '1234',
+      database: 'payfast'
+    });
+  }
+
+  if (process.env.NODE_ENV == 'production') {
+    return mysql.createConnection({
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PSWD,
+      database: process.env.DB_NAME
+    });
+  }
 }
 
 module.exports = function() {
